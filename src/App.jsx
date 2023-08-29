22afc0f82937ff2874371b0c60f26705f88821ff
repomann <rheimann87@gmail.com>
2023-08-28 18:30:08 -0700

    displaying products on 1 page

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './Home.jsx'
+import Login from './Login.jsx'
+// import SinglePage from './SinglePage.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div id="navbar">
+          <Link to='/login'>Login</Link>
+          <Link to='/register'>Register</Link>
+          <Link to='/cart'>Cart</Link>
+       </div>
+      <div id="main-section">
+        <h1>Enjoy your shopping experience!</h1>
+        <Routes>  
+          <Route path='/' element={<Home/>} />
+          {/* <Route path='/item/:id' element={<SinglePage/>}/>  */}
+          <Route path='/login' element={<Login/>}/>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
---
+++ src/Home.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react"
+import { fetchAllProducts } from "./api"
+
+
+
+function Home() {
+    const [merch, setMerch] = useState([])
+    const [error, setError] = useState(null)
+    const [searchParam, setSearchParam] = useState("");  // for Search Bar
+  
+    useEffect(() => {
+      async function getAllData() {
+        const response = await fetch('https://fakestoreapi.com/products')
+        const products = await response.json()
+        
+        setMerch(products)
+        console.log(merch)
+        console.log(products)
+
+        // const APIResponse = await fetchAllProducts();
+        // console.log(APIResponse.then(res=>res.json()))
+        // console.log("products")
+        // if (APIResponse.success) {
+        //     setMerch(APIResponse.json());
+        // } else {
+        //     setError(APIResponse.error.message);
+        // }
+      }
+      getAllData()
+    }, [])
+    
+
+    // search parameters and navigation - to be read, probably in another component
+    const productToDisplay = searchParam
+    ? merch.filter((title) =>
+        merch.title.toLowerCase().includes(searchParam)
+        ) : merch
+
+    return (
+    <div id="container">
+        {/* <pre>{JSON.stringify(merch, null, 2) }</pre> */}
+        {/* // search bar code
+        // <div>
+        //     <label>
+        //         search:{" "}
+        //         <input type="text" placeholder="search" onChange={(e) => setSearchParam(e.target.value.toLowerCase())}/>
+        //     </label>
+        // </div> */}
+
+        {/* <CreateProductForm merch={merch} setMerch={setMerch} /> */}
+        {error && <p>{error}</p>}
+        {merch.map((item) => {
+            return <div key={item.id}>
+                        {item.title} hello
+                    </div>;
+        })}
+
+{/* old code */}
+      {/* <div id="main-section">
+        {!!merch.length &&
+          merch.map((el, i) => {
+            return (
+              <div key={i}>
+                <h1>{el.id}</h1>
+              </div>
+            )
+          })}
+      </div> */}
+      
+      
+      </div>
+    )
+  }
+
+  export default Home
